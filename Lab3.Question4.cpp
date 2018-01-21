a419949571d848f086d8dabeaefab7a73a7b0109
NAVITALAL <31308073+NAVITALAL@users.noreply.github.com>
2018-01-21 09:55:16 +0000

    Update Lab3.Question4.cpp

--- Lab3.Question4.cpp
+++ Lab3.Question4.cpp
@@ -8,8 +8,12 @@ int main() {
 	int x;
 	cin>>x;
 	
-	if((x%5==0)&&(x%11==0)){cout<<"is divisible by 5 and 11";}
-	else{cout<<"is not divisible by 5 and 11";}
+	if( (x%5==0) && (x%11==0) ){
+		cout<<"is divisible by 5 and 11";
+	}
+	else{
+		cout<<"is not divisible by 5 and 11";
+	}
 	
 	return 0;
 }
